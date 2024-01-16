bc839f6633dc23c97d7de027d5e2843ec6aeb9ff
Svetlana <svetlana.moiseienko.web@gmail.com>
2024-01-16 15:51:48 +0400

    feat: implement slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.css';
+import Swiper from 'swiper';
+import { Navigation, Pagination, EffectFade, Autoplay } from 'swiper/modules';
+Swiper.use([Navigation, Pagination, EffectFade, Autoplay]);
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade'
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    effect: 'fade',
+    speed: 1000,
+    loop: true,
+
+    autoplay: true,
+
+    fadeEffect: {
+        crossFade: true,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+});
